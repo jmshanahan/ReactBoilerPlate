c0da6b7d2d29c1b13654dd4b70a472b947c19660
jmshanahan <comeraghsolutions@gail.com>
2020-03-12 11:57:27 +0000

    Fixed naming issue with webpack.comfig.base

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
 const merge = require('webpack-merge')
-const baseConfig = require('./webpack.config.base.')
+const baseConfig = require('./webpack.config.base')
 module.exports = merge(baseConfig, {
   mode: 'development',
   devServer: { port: 9000 },
